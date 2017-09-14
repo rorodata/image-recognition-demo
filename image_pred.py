572c727b57e843a6fb8c6bea8450297e206c2cec
akrishnamo <ananth.krishnamoorthy@gmail.com>
2017-09-14 09:28:48 +0530

    AK Updates for demo
    changes function Classify to Predict
    changed code to load VGG only once

--- image_pred.py
+++ image_pred.py
@@ -7,7 +7,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-vgg16=keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None)
+#vgg16=keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None)
 
 # b=plt.imread('/data/animal1.jpg')
 
@@ -18,10 +18,23 @@ def topk(pred, k=3):
     topk.reverse()
     return topk
 
-def classify(image):
+_model = None
+
+def load_model():
+    global _model
+    if _model is None:
+        _model = keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None)
+    return _model
+
+def predict(image, k=5):
+    """
+    input - takes an RGB image (numpy array ) as input, preferably bigger than 224X224
+    returns - json file with two lists: Label, Probability
+    """
     img=scipy.misc.imresize(image, (224,224))
+    vgg16=load_model()
     pred=vgg16.predict(img.reshape(1,224,224,3))
-    top5=topk(pred,5)
+    top5=topk(pred,k=5)
 #     for i in top5:
 #         print(pred[0,i], imagenet_labels.imgnet1000[i])
     labelz=[imagenet_labels.imgnet1000[i] for i in top5]
@@ -31,5 +44,5 @@ def classify(image):
 
 if __name__=='__main__':
     image=plt.imread(sys.argv[1])
-    result = classify(image)
+    result = predict(image)
     print(result)
